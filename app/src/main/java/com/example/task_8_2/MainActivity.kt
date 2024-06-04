73c50c7c1da7181e89625c81bb20628b7562e947
azotGr <alinazotova721@gmail.com>
2024-06-04 05:13:21 +0300

    /

--- app/src/main/java/com/example/task_8_2/MainActivity.kt
+++ app/src/main/java/com/example/task_8_2/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.task_8_2
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
